23c794378182ba32c99251ab65b7b4739805b93b
Chema Muñoz <chemunoz78@gmail.com>
2023-08-05 09:20:06 +0200

    feat: add data bind and validation in register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registroForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  get nombreValid() {
+    return this.registroForm.get('nombre')?.valid;
+  }
+
+  get correoValid() {
+    return this.registroForm.get('correo')?.valid;
+  }
+
+  get passwordValid() {
+    return this.registroForm.get('password')?.valid;
+  }
+
+  public crearUsuario(): void {
+    console.log(this.registroForm.valid);
+    console.log(this.registroForm.value);
+  }
 }
