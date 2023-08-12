54a67ded6a5eb96b6f8b069439a8155629c2895d
Chema Muñoz <chemunoz78@gmail.com>
2023-08-13 00:21:50 +0200

    chore: add and set up firebase dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -28,7 +32,14 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     NavbarComponent,
     SidebarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'redux-ingreso-egreso-app-1ef5e',
+    appId: '1:928386082743:web:4dffa850456b28a95faa32',
+    storageBucket: 'redux-ingreso-egreso-app-1ef5e.appspot.com',
+    apiKey: 'AIzaSyDxeD0-XnoWoX5YSSdTGP527c9taRMo1fo',
+    authDomain: 'redux-ingreso-egreso-app-1ef5e.firebaseapp.com',
+    messagingSenderId: '928386082743',
+    measurementId: 'G-G4724Q42E4',
+  },
+};
