be863f31a7ecd509bf86fc9a795b20c4b745779c
Chema Muñoz <chemunoz78@gmail.com>
2023-08-14 01:18:44 +0200

    feat: add auth user info for each action

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import {
   Auth,
+  authState,
   UserCredential,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  User,
 } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private auth: Auth) {}
+
+  initAuthListener(): void {
+    authState(this.auth).subscribe((fuser: User | null) => console.log(fuser));
+  }
 
   crearUsuario(email: string, password: string): Promise<UserCredential> {
     return createUserWithEmailAndPassword(this.auth, email, password);
