e6e4376607c3526b25c0442a2bfa8b6238b18f19
Chema Muñoz <chemunoz78@gmail.com>
2023-08-14 00:36:52 +0200

    feat: add sweetalert2 dependency

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -35,13 +36,25 @@ export class LoginComponent implements OnInit {
   public loginUsuario(): void {
     if (this.loginForm.invalid) return;
 
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => Swal.showLoading(),
+    });
+
     const { correo, password } = this.loginForm.value;
     this.authService
       .loginUsuario(correo, password)
       .then((credenciales) => {
         console.log(credenciales);
+        Swal.close();
         this.router.navigate(['/']);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message,
+        });
+      });
   }
 }
